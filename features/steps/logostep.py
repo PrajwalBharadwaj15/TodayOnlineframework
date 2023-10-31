522642eee7c57b67b486b01861deec8b81388f3b
PrajwalBharadwaj15 <149361028+PrajwalBharadwaj15@users.noreply.github.com>
2023-10-31 11:54:24 +0530

    updated feature code

--- features/steps/logostep.py
+++ features/steps/logostep.py
@@ -21,8 +21,8 @@ def open_home_page(context):
 def verify_logo_presence(context):
     try:
         # Use a CSS selector to locate the logo element on the home page.
-        logo_link = context.driver.find_element(By.XPATH, '//a[@class="logo-link"]//img[@src="/themes/custom/mc_todayonline_theme/images/logo.svg"]')
-
+        logo_link = context.driver.find_element(By.XPATH,'//a[@class="logo-link"]')
+# can also add this with above xpath//img[@src="/themes/custom/mc_todayonline_theme/images/logo.svg"]
         # Check if the logo is displayed
         assert logo_link.is_displayed()
     except Exception as e:
