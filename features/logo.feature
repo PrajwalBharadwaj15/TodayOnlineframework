cd0d79e5cc93556bc637373dba929f50a211d1ae
PrajwalBharadwaj15 <149361028+PrajwalBharadwaj15@users.noreply.github.com>
2023-10-31 11:34:36 +0530

    modified code feature code

--- features/logo.feature
+++ features/logo.feature
@@ -3,5 +3,5 @@ Feature: Today logo
     Scenario: Identify today logo in home page
      Given Launch chrome browser
      When Open home page
-     Then Veify logo presence and click
-     And Exit chome browser 
+     Then Verify logo presence
+     And Exit chrome browser 
--- features/steps/logostep.py
+++ features/steps/logostep.py
@@ -2,31 +2,34 @@ from behave import given, when, then
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
 # Initialize the WebDriver
+global driver
 driver = None
 
-@given('Launch chrome browser')
+@given(u'Launch chrome browser')
 def launch_browser(context):
+    
     context.driver = webdriver.Chrome()
 
-@when('Open home page')
+@when(u'Open home page')
 def open_home_page(context):
     # Navigate to the URL of your home page.
     context.driver.get('https://www.todayonline.com/')  # Replace with the actual URL.
 
-@then('Verify logo presence and click')
-def verify_logo_and_click(context):
-    # Use a CSS selector to locate the logo element on the home page.
-    logo = context.driver.find_element(By.CSS_SELECTOR, 'logo__image')  # Replace with the actual CSS selector for the logo.
+@then('Verify logo presence')
+def verify_logo_presence(context):
+    try:
+        # Use a CSS selector to locate the logo element on the home page.
+        logo_link = context.driver.find_element(By.XPATH, '//a[@class="logo-link"]//img[@src="/themes/custom/mc_todayonline_theme/images/logo.svg"]')
 
-    # Check if the logo element is displayed.
-    if logo.is_displayed():
-        # If the logo is displayed, click on it.
-        logo.click()
-    else:
-        raise AssertionError('Logo not found or not visible')
+        # Check if the logo is displayed
+        assert logo_link.is_displayed()
+    except Exception as e:
+        raise AssertionError('Logo not found or not visible: {}'.format(e))
 
-@then('Exit chrome browser')
+    
+@then(u'Exit chrome browser')
 def exit_browser(context):
     # Close the Chrome browser.
     context.driver.quit()
