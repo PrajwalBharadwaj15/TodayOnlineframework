48651ae7eb46e1b3eadb1737e0b0880ff3ba7f02
PrajwalBharadwaj15 <149361028+PrajwalBharadwaj15@users.noreply.github.com>
2023-11-02 14:28:04 +0530

    updated btt xpath

--- features/steps/Bttbuttonstep.py
+++ features/steps/Bttbuttonstep.py
@@ -8,7 +8,9 @@ def click_first_article(context):
     try:
         
        wait = WebDriverWait(context.driver, 5)
-       first_article = wait.until(EC.element_to_be_clickable((By.XPATH, "//body/div[1]/div[1]/div[2]/main[1]/div[1]/section[1]/article[1]/div[1]/div[3]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]/picture[1]/img[1]")))
+       #use /*postion() = number to select the element in specific position
+       first_article = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='middle-9s-5p__items middle-9s-5p__items--col-one']/*[position()=1]")))
+       #//body/div[1]/div[1]/div[2]/main[1]/div[1]/section[1]/article[1]/div[1]/div[3]/div[1]/section[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]/picture[1]/img[1]
        first_article.click()
     except Exception as e:
         raise AssertionError('First article not found or not visible: {}'.format(e))
