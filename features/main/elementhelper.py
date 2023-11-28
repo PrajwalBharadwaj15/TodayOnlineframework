67e980c21642feb649ba875d0e024ea7bc752e00
PrajwalBharadwaj15 <149361028+PrajwalBharadwaj15@users.noreply.github.com>
2023-11-28 15:49:33 +0530

    added scroll helper

--- features/main/elementhelper.py
+++ features/main/elementhelper.py
@@ -1,10 +1,11 @@
 from selenium.common.exceptions import NoSuchElementException
 
+
 class elementHandler:
-    def fn_click(self, elem):
+    def fn_click(context, elem):
         bln_status = True
         try:
-            if self.fn_is_element_present(elem):
+            if context.fn_is_element_present(elem):
                 elem.click()
             else:
                 raise NoSuchElementException("Element Not Present")
@@ -12,8 +13,13 @@ class elementHandler:
             bln_status = False
         return bln_status
 
-    def fn_is_element_present(self, elem):
+    def fn_is_element_present(context, elem):
         try:
             return elem is not None and elem.is_displayed()
         except NoSuchElementException:
             return False
+    
+    def fn_scrolltoviewbtn(context):
+        scroll = context.execute_script("window.scrollTo(0,4500);")
+        return scroll
+        
